6f0b7f20b3c50c83962af2a6d2f44887c14bd608
iamalan <aagon86@gmail.com>
2013-04-03 11:10:40 +1030

    New state change functionality. More general case coming soon.

--- Game Scripts/TerrainGen.js
+++ Game Scripts/TerrainGen.js
@@ -17,37 +17,48 @@ var isSpawned : boolean;
 public var familyOneTerrains : GameObject[];
 public var familyTwoTerrains : GameObject[];
 public var destroyPoints : GameObject[];
+//This number is used to decide WHEN to change - its a user input choose whole numbers
+public var familyStateChange : int;
 
 //A queue seemed like the most logical idea here seeing as were FIFO for terrains
 public var platformQueue : Queue;
-public var terrainPool : GameObject[];
-
+public var terrainPoolOne : GameObject[];
+public var terrainPoolTwo : GameObject[];
+public var terrainPoolThree : GameObject[];
 
 //We'll be basing our terrain family state on number of generated terrains so far (not including the generated terrains in Awake()), e.g. if we are above say 5 then use familyTwoTerrains
-private static var generatedSoFar = 0;
+public static var generatedSoFar = 0;
 
-private var waitOne : boolean;
+private var terrainOneOn : boolean = true;
 
 //Much cleaner when instantiated in awake
 function Awake() {
-
-	//TODO Add first platform back in
 	
 	if(numberOfPlatformsAhead>numberOfPlatformsRepeats*familyOneTerrains.Length){
 		numberOfPlatformsAhead = numberOfPlatformsRepeats*familyOneTerrains.Length-1;
 	}
-	//Make the terrainPool. This will hold all the possible terrains however they will all be inactive. In start we will chose numberOfPlatformsAhead number to activate
- 	terrainPool = new GameObject[numberOfPlatformsRepeats*familyOneTerrains.Length]; 
+	//Make the terrainPoolOne. This will hold all the possible terrains however they will all be inactive. In start we will chose numberOfPlatformsAhead number to activate
+ 	terrainPoolOne = new GameObject[numberOfPlatformsRepeats*familyOneTerrains.Length]; 
 	var shifter : int = 0;
 	for ( var i=0; i<familyOneTerrains.Length; i++ ) {
 			for ( var j=0; j<numberOfPlatformsRepeats; j++ ){
 				var tempPlat = Instantiate(familyOneTerrains[i], Vector3(player.transform.position.x,0,player.transform.position.z + shifter*10), Quaternion.Euler(0, 0, 0));
-				terrainPool[numberOfPlatformsRepeats*i+j]= tempPlat;
-				terrainPool[numberOfPlatformsRepeats*i+j].active = false;
+				terrainPoolOne[numberOfPlatformsRepeats*i+j]= tempPlat;
+				terrainPoolOne[numberOfPlatformsRepeats*i+j].active = false;
 				shifter++;
 			}
 	}
 	
+	//Make the terrainPoolTwo. This will hold all the possible terrains however they will all be inactive. In start we will chose numberOfPlatformsAhead number to activate
+ 	terrainPoolTwo = new GameObject[numberOfPlatformsRepeats*familyTwoTerrains.Length]; 
+	for ( i=0; i<familyTwoTerrains.Length; i++ ) {
+			for ( j=0; j<numberOfPlatformsRepeats; j++ ){
+				tempPlat = Instantiate(familyTwoTerrains[i], Vector3(player.transform.position.x,0,player.transform.position.z + shifter*10), Quaternion.Euler(0, 0, 0));
+				terrainPoolTwo[numberOfPlatformsRepeats*i+j]= tempPlat;
+				terrainPoolTwo[numberOfPlatformsRepeats*i+j].active = false;
+				shifter++;
+			}
+	}
 	isSpawned = true; 
 } 
 
@@ -55,19 +66,19 @@ function Start() {
 
 	platformQueue = new Queue();
 	
-	//Activate random terrains in terrainPool
+	//Activate random terrains in terrainPools
 	
 	for ( var i=0; i<numberOfPlatformsAhead; i++ ) {
 		if ( i==0 ) {
-			var tempPlat = terrainPool[0];
+			var tempPlat = terrainPoolOne[0];
 			tempPlat.active = true;
 			destroyPoints = GameObject.FindGameObjectsWithTag("DestroyPoint");
 			tempPlat.transform.position = Vector3(player.transform.position.x,0,player.transform.position.z);
 			platformQueue.Enqueue(tempPlat);
 		} else {
-			tempPlat = terrainPool[Random.Range(1, terrainPool.Length)];
+			tempPlat = terrainPoolOne[Random.Range(1, terrainPoolOne.Length)];
 			while(tempPlat.active == true){
-				tempPlat = terrainPool[Random.Range(1, terrainPool.Length)];
+				tempPlat = terrainPoolOne[Random.Range(1, terrainPoolOne.Length)];
 			}
 			destroyPoints = GameObject.FindGameObjectsWithTag("DestroyPoint");
 			tempPlat.transform.position = destroyPoints[FindFurthest()].transform.position;
@@ -75,23 +86,36 @@ function Start() {
 			platformQueue.Enqueue(tempPlat);
 		}
 	}
+	
+	generatedSoFar++;
 }
-
-
 
 function Update() {
 
 	destroyPoints = GameObject.FindGameObjectsWithTag("DestroyPoint");
 
-	if ( generatedSoFar == 5 ){
+	//Every familyStateChange number of times, we switch to using other terrainPool
+	if ( generatedSoFar%familyStateChange == 0 ){
 		//Congratulate user?
-		//TODO: Family state change
+		UpdateFamilyState();
+		generatedSoFar++;
 	}
 		
-	if( !isSpawned ){
-		var tempPlat = terrainPool[Random.Range(1, terrainPool.Length)];
+	if( !isSpawned && terrainOneOn ){
+		var tempPlat = terrainPoolOne[Random.Range(1, terrainPoolOne.Length)];
 		while(tempPlat.active == true){
-			tempPlat = terrainPool[Random.Range(1, terrainPool.Length)];
+			tempPlat = terrainPoolOne[Random.Range(1, terrainPoolOne.Length)];
+		}
+		tempPlat.transform.position = destroyPoints[FindFurthest()].transform.position;
+		tempPlat.active = true;
+		isSpawned= true;
+		
+		generatedSoFar++;
+	} else if(!isSpawned && !terrainOneOn) {
+		
+		tempPlat = terrainPoolTwo[Random.Range(0, terrainPoolTwo.Length)];
+		while(tempPlat.active == true){
+			tempPlat = terrainPoolTwo[Random.Range(0, terrainPoolTwo.Length)];
 		}
 		tempPlat.transform.position = destroyPoints[FindFurthest()].transform.position;
 		tempPlat.active = true;
@@ -100,11 +124,14 @@ function Update() {
 		generatedSoFar++;
 	}
 
+}
+
+
+function LateUpdate(){
 	if( player.transform.position.z > destroyPoints[FindClosest()].transform.position.z + cameraThreshold ){
 		destroyPoints[FindClosest()].gameObject.transform.parent.active= false;
 		isSpawned= false;
 	}
-
 }
 
 function FindFurthest() {
@@ -141,4 +168,19 @@ function FindClosest() {
     }
     
     return indexSmallest;
+}
+
+function GenerateTokens() {
+
+
+}
+
+function UpdateFamilyState() {
+	Debug.Log("Just changed state");
+	if (terrainOneOn){
+		terrainOneOn = false;
+	} else{
+		terrainOneOn = true;
+	}
+	
 }
